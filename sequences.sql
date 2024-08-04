1fe1e82bb0eb51aa32b283a3c9a5675f64bfb755
Sofia Golovina <65851895+Zonichka@users.noreply.github.com>
2024-08-04 18:35:52 +0500

    Create sequences.sql
    Определения последовательностей

---
+++ sequences.sql
@@ -0,0 +1,54 @@
+create sequence seq_color
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+create sequence seq_wood
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+
+create sequence seq_cust
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+create sequence seq_material
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+create sequence seq_prod
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+create sequence seq_man
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+create sequence seq_warehouse
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+create sequence seq_delivery
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
+
+create sequence seq_sale
+as integer
+minvalue 0
+maxvalue 9999
+increment by 1;
